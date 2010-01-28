41d17e8f4010c2fe4cba140dbe3a5add6074264d
Michael Stillwell <mjs@beebo.org>
2010-01-28 23:22:14 +0000

    Switch to functional style.

--- router-run.js
+++ router-run.js
@@ -3,64 +3,54 @@ var sys = require('sys'),
     router = require('./router'),
     url = require('url');
 
-function MyRouter(obj, req, res) {
-    sys.debug("obj = " + sys.inspect(obj));
-    sys.debug("req = " + sys.inspect(req));
-    sys.debug("res = " + sys.inspect(res));
-    this.obj = obj;
-    this.req = req;
-    this.res = res;
-    this.url = url.parse(req.url, true)
-    sys.debug("this = " + sys.inspect(this));
+function MyRouter(name) {
+    this.name = name;
 }
 
 MyRouter.prototype = {
 
-    greet: function() {
-        return "Hello, " + this.obj.name;
+    send: function(res) {
+        res.sendHeader(200, {"Content-Type": "application/json"});
+        res.sendBody(JSON.stringify(this.name) + "\n");
+        res.finish();
     },
 
-    // curl -s http://127.0.0.1:8000/name
+    // curl -i -s http://127.0.0.1:8000/user/mjs
 
-    "GET /name": function() {
+    "GET /user/(.*)": function(req, res, matches) {
 
         var body = "", self = this;
 
-        self.req.addListener("body", function(s) {
+        req.addListener("body", function(s) {
             body += s;
         });
 
-        self.req.addListener("complete", function() {
-            self.res.sendHeader(200, {"Content-Type": "application/json"});
-            self.res.sendBody(JSON.stringify(self.greet()) + "\n");
-            self.res.finish();
+        req.addListener("complete", function() {
+            self.send(res);
         });
 
     },
 
-    // curl -s -X PUT -d '"Michael"' http://127.0.0.1:8000/name
+    // curl -i -s -X PUT -d '"Michael"' http://127.0.0.1:8000/user/mjs
 
-    "PUT /name": function() {
+    "PUT /user/(.*)": function(req, res, matches) {
 
         var body = "", self = this;
 
-        self.req.addListener("body", function(s) {
+        req.addListener("body", function(s) {
             body += s;
         });
         
-        self.req.addListener("complete", function() {
-            self.obj.name = JSON.parse(body); // Assumes body is JSON
-            sys.debug("Setting name to " + self.obj.name);
-            self.res.sendHeader(200, {"Content-Type": "application/json"});
-            self.res.sendBody(JSON.stringify(self.greet()) + "\n");
-            self.res.finish();
+        req.addListener("complete", function() {
+            self.name = JSON.parse(body); // Assumes body is JSON
+            sys.debug("Setting name to " + self.name);
+            self.send(res);
         });
 
     }
 
 };
 
-http.createServer(router.createSimple(MyRouter, {})).listen(8000);
+http.createServer(router.createSimple(new MyRouter("Clem"))).listen(8000);
 
 sys.puts('Server running at http://127.0.0.1:8000/');
-
--- router.js
+++ router.js
@@ -5,15 +5,9 @@ exports.createSimple = function(rules) {
 
     var regexp = /^\s*((\S*)\s+)?(\S*)\s*$/, parts = null, matchers = {};
 
-    args = Array.prototype.slice.call(arguments, 1);
-
-    for (r in rules.prototype) {
-
-//        sys.debug("r = " + r);
+    for (r in rules) {
 
         parts = r.match(regexp);
-
-//        sys.debug(sys.inspect(parts));
 
         if (!matchers[parts[2]]) {
             matchers[parts[2]] = [];
@@ -21,7 +15,7 @@ exports.createSimple = function(rules) {
 
         matchers[parts[2]].push([
             new RegExp(parts[3]),
-            rules.prototype[r]
+            rules[r]
         ]);
 
     }
@@ -32,21 +26,13 @@ exports.createSimple = function(rules) {
 
         for (i = 0, l = m.length; i < l; i++) {
             if ((parts = url.parse(req.url).pathname.match(m[i][0]))) {
-                sys.debug(req.method + " " + req.url + " OK");
-
-                sys.debug("a = " + sys.inspect(rules.prototype.constructor.toString()));
-                var r = rules.prototype.constructor.apply(null, args.concat(req, res));
-                                
-//                new rules(req, res, parts);
-
-
-                sys.debug(sys.inspect(m[i]));
-                m[i][1].call(r);
+//                sys.debug(req.method + " " + req.url + " OK");
+                m[i][1].call(rules, req, res, parts);
                 return;
             }
         }
 
-        sys.debug(req.method + " " + req.url + " Not Found");
+//        sys.debug(req.method + " " + req.url + " Not Found");
 
         res.sendHeader(404);
         res.finish();
