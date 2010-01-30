45edc827494376d2554deb6a1f32d0539c21e33b
Michael Stillwell <mjs@beebo.org>
2010-01-30 00:58:45 +0000

    reformatting

--- hydra-run.js
+++ hydra-run.js
@@ -10,11 +10,9 @@
 //
 //   {"type":"response",body:"jjjj","id":"36057424"}
 
-var http = require("http");
-
-var ws = require("./ws"); // Get ws.js from http://github.com/ncr/node.ws.js
-
-var hydra = require("./hydra");
+var http = require("http"),
+    ws = require("./ws"), // Get ws.js from http://github.com/ncr/node.ws.js
+    hydra = require("./hydra");
 
 var server = hydra.create(http.createServer, ws.createServer);
 
