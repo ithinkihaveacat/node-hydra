98b56be5d70c38c51b14b856d548aa88068a0942
Michael Stillwell <mjs@beebo.org>
2010-02-02 00:28:33 +0000

    use ejsgi for static server

--- run.js
+++ run.js
@@ -4,22 +4,22 @@
 //
 // Server request:
 //
-//   $ http_proxy=http://127.0.0.1:8000/ wget -q -S -O - http://client5f99eaa4/
+//   $ http_proxy=http://127.0.0.1:8081/ wget -q -S -O - http://client5f99eaa4/
 //
 // ... client response:
 //
 //   {"type":"response",body:"jjjj","id":"36057424"}
 
-var http = require("http"),
-    sys = require('sys'),
+DEBUG = true;
+
+var sys = require('sys'),
+    http = require('http'),
+    ejsgi = require("../ejsgi/lib/ejsgi"),
     ws = require("./lib/ws"), // Get ws.js from http://github.com/ncr/node.ws.js
     hydra = require("./lib/hydra"),
     router = require("./lib/router");
-
-DEBUG = false;
-
-http.createServer(router.create(new router.Static("htdocs"))).listen(8080);
+    
+ejsgi.Server(new router.Static("htdocs").dispatcher(), "localhost", 8080).start();
 sys.puts("Static Httpd listening at http://127.0.0.1:8080/");
 
 hydra.create(http.createServer, ws.createServer).listen(8081, 8082);
-
